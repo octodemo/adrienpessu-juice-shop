69dbde05a4afda4bdccac8efb65e78531e3324fb
Adrien Pessu <7055334+adrienpessu@users.noreply.github.com>
2024-02-02 11:49:12 +0100

    remove boilerplate

--- routes/sqli.ts
+++ routes/sqli.ts
@@ -1,7 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 const { Pool } = require('pg')
-var RateLimit = require('express-rate-limit')
 
 const pool = new Pool({
     user: 'dbuser',
@@ -11,20 +10,11 @@ const pool = new Pool({
     port: 3211,
 })
 
-var test;
 var app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
-var limiter = RateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-});
-
-// apply rate limiter to all requests
-app.use(limiter);
 
 
 app.get("/", function(req, res){
@@ -42,7 +32,3 @@ app.get("/", function(req, res){
 app.listen(8000, function () {
     console.log("Server running");
 });
-
-  let drinks = ['lemonade', 'soda', 'tea', 'water'];
-  let food = ['beans', 'chicken', 'rice'];
-  let iban = "DE012345678910112345"
